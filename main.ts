ca80abf64d5dca5c68ae6a1403afe9e117ec0bf8
Daniel Dye <dnitemaeric@gmail.com>
2024-10-17 10:58:20 +0100

    Move DR installation instead of copying

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import { Command } from "npm:commander";
-import { copy } from "jsr:@std/fs";
+import { copy, move } from "jsr:@std/fs";
 
 import global from "./commands/global.ts";
 
@@ -22,12 +22,12 @@ const actionRunner = (fn: (...args: any[]) => Promise<any>) => {
 
 program
   .name("drenv")
-  .description("CLI to manage DragonRuby environments")
-  .version("0.1.0");
+  .description("CLI to manage DragonRuby environments.")
+  .version("0.1.1");
 
 program.command("new")
   .argument("<name>", "Name of the new project")
-  .description("Create a new DragonRuby project")
+  .description("Create a new DragonRuby project.")
   .action(async (name) => {
     await copy(
       `${Deno.env.get("HOME")}/.drenv/versions/${await global()}`,
@@ -37,21 +37,21 @@ program.command("new")
 
 program.command("register")
   .argument("<path>", "Path to a fresh DragonRuby directory")
-  .description("Register a DragonRuby installation")
+  .description("Register a DragonRuby installation. This moves the installation to the $HOME/.drenv directory.")
   .action(async (path) => {
     const content = await readFirstLine(path + "/CHANGELOG-CURR.txt");
     const version = content.match(/[0-9\.]+/)?.[0];
 
-    await copy(path, `${Deno.env.get("HOME")}/.drenv/versions/${version}`);
+    await move(path, `${Deno.env.get("HOME")}/.drenv/versions/${version}`);
   });
 
 program.command("global")
   .argument("[version]", "Version of DragonRuby to use")
-  .description("Get or set the global version of DragonRuby")
+  .description("Get or set the global version of DragonRuby.")
   .action(actionRunner(global));
 
 program.command("versions")
-  .description("List out all locally installed versions of DragonRuby")
+  .description("List out all locally installed versions of DragonRuby.")
   .action(async (options) => {
     const directories = await Deno.readDir(
       `${Deno.env.get("HOME")}/.drenv/versions/`,
