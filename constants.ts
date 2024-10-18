b524dc950450fa26b6afc107a44a69eead30c989
Daniel Dye <dnitemaeric@gmail.com>
2024-10-18 16:42:32 +0100

    Fix release workflow

--- constants.ts
+++ constants.ts
@@ -1,4 +1,4 @@
-export const version = "0.3.3";
+export const version = "0.3.4";
 export const homePath = `${Deno.env.get("HOME")}/.drenv`;
 export const versionsPath = `${homePath}/versions`;
 export const binPath = `${homePath}/bin`;
