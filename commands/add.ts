3c1c83fa8781a5665f6426c7992ce412ac3b8106
Daniel Dye <dnitemaeric@gmail.com>
2025-01-31 15:00:27 +0000

    Format commands/add.ts

--- commands/add.ts
+++ commands/add.ts
@@ -1,23 +1,30 @@
-import { ensureDir } from "@std/fs"
+import { ensureDir } from "@std/fs";
 
 export default async function add(recipe: string) {
   switch (recipe) {
     case "foodchain":
-      return foodchain();
+      return await foodchain();
     default:
       throw new Error(`Unknown recipe: ${recipe}`);
   }
 }
 
 const foodchain = async () => {
-  console.log("Installing foodchain...")
+  console.log("Installing foodchain...");
 
-  const foodchainUrl = "https://raw.githubusercontent.com/pvande/foodchain/refs/heads/main/foodchain.rb"
+  const foodchainUrl =
+    "https://raw.githubusercontent.com/pvande/foodchain/refs/heads/main/foodchain.rb";
 
-  const response = await fetch(foodchainUrl)
-  const foodchainText = await response.text()
+  const response = await fetch(foodchainUrl);
+  const foodchainText = await response.text();
 
-  await ensureDir("mygame/vendor/pvande/foodchain")
-  await Deno.writeTextFile("mygame/vendor/pvande/foodchain/foodchain.rb", foodchainText)
-  await Deno.writeTextFile("mygame/dependencies.rb", "require \"vendor/pvande/foodchain/foodchain.rb\"\n")
-}
+  await ensureDir("mygame/vendor/pvande/foodchain");
+  await Deno.writeTextFile(
+    "mygame/vendor/pvande/foodchain/foodchain.rb",
+    foodchainText,
+  );
+  await Deno.writeTextFile(
+    "mygame/dependencies.rb",
+    'require "vendor/pvande/foodchain/foodchain.rb"\n',
+  );
+};
