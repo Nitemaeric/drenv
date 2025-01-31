6e888f8e1a7fedeaf2029327dd35e012839c294a
Daniel Dye <dnitemaeric@gmail.com>
2025-01-31 17:29:04 +0000

    Add "install [tier]" command

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
-import { Command } from "npm:commander";
+import { Argument, Command } from "commander";
+
 import config from "./deno.json" with { type: "json" };
 
 import add from "./commands/add.ts";
 import global from "./commands/global.ts";
+import install from "./commands/install.ts";
 import local from "./commands/local.ts";
 import newCommand from "./commands/new.ts";
 import register from "./commands/register.ts";
@@ -12,17 +14,16 @@ import versions from "./commands/versions.ts";
 
 export const program = new Command();
 
-const actionRunner = (fn: (...args: any[]) => Promise<any>) => {
-  return (...args: any[]): any =>
+const actionRunner = (fn: (...args: string[]) => Promise<unknown>) => {
+  return (...args: string[]): void | Promise<void> =>
     fn(...args)
       .then(
-        (value) =>
-          (typeof value === "string" ||
-            typeof value === "number") && console.log(value),
+        (value) => {
+          (typeof value === "string" || typeof value === "number") &&
+            console.log(value);
+        },
       )
-      .catch(
-        (error) => console.error(error.message),
-      );
+      .catch((error) => console.error(error.message));
 };
 
 program
@@ -30,16 +31,19 @@ program
   .description("CLI to manage DragonRuby environments")
   .version(config.version);
 
-program.command("setup")
+program
+  .command("setup")
   .description("Setup your shell profile to use drenv")
   .action(actionRunner(setup));
 
-program.command("new")
+program
+  .command("new")
   .argument("<name>", "Name of the new project")
   .description("Create a new DragonRuby project")
   .action(actionRunner(newCommand));
 
-program.command("register")
+program
+  .command("register")
   .argument("<path>", "Path to a fresh DragonRuby directory")
   .summary("Register a DragonRuby installation")
   .description(
@@ -47,27 +51,42 @@ program.command("register")
   )
   .action(actionRunner(register));
 
-program.command("add")
+program
+  .command("add")
   .argument("<recipe>", "Name of the recipe to add")
   .description("Setup a pre-configured library")
   .action(actionRunner(add));
 
-program.command("global")
+program
+  .command("global")
   .argument("[version]", "Version of DragonRuby to use")
   .description("Get or set the global version of DragonRuby")
   .action(actionRunner(global));
 
-program.command("local")
+program
+  .command("local")
   .argument("[version]", "Version of DragonRuby to use")
   .description("Get or set the local version of DragonRuby")
   .action(actionRunner(local));
 
-program.command("versions")
+program
+  .command("versions")
   .description("List out all locally installed versions of DragonRuby")
   .action(actionRunner(versions));
 
-program.command("upgrade")
+program
+  .command("upgrade")
   .description("Upgrade the version of drenv")
   .action(actionRunner(upgrade));
+
+program
+  .command("install")
+  .addArgument(
+    new Argument("[tier]", "Tier of DragonRuby to install")
+      .choices(["standard", "indie", "pro"])
+      .default("standard"),
+  )
+  .description("Install the latest version of drenv")
+  .action(actionRunner(install));
 
 program.parse();
